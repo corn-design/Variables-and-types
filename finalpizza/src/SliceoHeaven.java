2b50bd8559b7bc06fb0818893272a8762c8eff30
corn-design <3495682912@qq.com>
2025-03-08 21:31:19 +0800

    7

--- finalpizza/src/SliceoHeaven.java
+++ finalpizza/src/SliceoHeaven.java
@@ -50,23 +50,86 @@ public class SliceoHeaven {
     }
 
     public void takeOrder() {
+        int ingChoice1, ingChoice2, ingChoice3;
+        boolean allChoicesValid;
+        int sizeChoice;
+        int sideDishChoice;
         Scanner input = new Scanner(System.in);
+        
+        do{
+            System.out.println("Please pick any three of the following ingredients:\r\n" + //
+                        " 1. Mushroom\r\n" + //
+                        " 2. Paprika\r\n" + //
+                        " 3. Sun-dried tomatoes\r\n" + //
+                        " 4. Chicken\r\n" + //
+                        " 5. Pineapple\r\n" + //
+                        " Enter any three choices (1, 2, 3,…) separated by spaces:");
+        ingChoice1 = input.nextInt();
+        ingChoice2 = input.nextInt();
+        ingChoice3 = input.nextInt(); 
+        if (!isValidChoice(ingChoice1)) {
+            allChoicesValid = false;
+        }
+        if (!isValidChoice(ingChoice2)) {
+            allChoicesValid = false;
+        }
+        if (!isValidChoice(ingChoice3)) {
+            allChoicesValid = false;
+        }
 
-        System.out.println("Enter three ingredients for your pizza (use spaces to separate ingredients):");
-        String[]ingredients = input.nextLine().split("");
-        String ing1 = ingredients[0];
-        String ing2 = ingredients[1];
-        String ing3 = ingredients[2];    
-
-
-        System.out.println("Enter size of pizza (Small, Medium, Large):");
-        String pizzaSize = input.next();
+        if (!allChoicesValid) {
+            System.out.println("Invalid choice(s). Please pick only from the given list:");
+        } 
+        }
+        while(allChoicesValid);
+            String ing1 = convertChoice(ingChoice1);
+            String ing2 = convertChoice(ingChoice2);
+            String ing3 = convertChoice(ingChoice3);
+            System.out.println("You have chosen: " + ing1 + ", " + ing2 + ", " + ing3);
+         
+        do{
+        System.out.println("What size should your pizza be?\r\n" + //
+                        " 1. Large\r\n" + //
+                        " 2. Medium\r\n" + //
+                        " 3. Small\r\n" + //
+                        " Enter only one choice (1, 2, or 3):");
+        sizeChoice = input.nextInt();
+         if (!isValidChoice2(sizeChoice)) {
+            System.out.println("Invalid choice(s). Please pick only from the given list:");
+            allChoicesValid = false;
+        }
+        while(allChoicesValid);
+        String pizzaSize = convertSideChoice(sizeChoice);
+       
         
         System.out.println("Do you want extra cheese (Y/N):");
         String extraCheese = input.nextLine();
+        
+        do{
+        System.out.println("Following are the side dish that go well with your pizza:\r\n" + //
+                        " 1. Calzone\r\n" + //
+                        " 2. Garlic bread\r\n" + //
+                        " 3. Chicken puff\r\n" + //
+                        " 4. Muffin\r\n" + //
+                        " 5. Nothing for me\r\n" + //
+                        " What would you like? Pick one (1, 2, 3,…):");
+        sideDishChoice = input.nextInt();
+        if (!isValidChoice(sideDishChoice)) {
+            System.out.println("Invalid choice(s). Please pick only from the given list:");
+            allChoicesValid = false;
+        }
+        while(allChoicesValid);
+        String sideDish = convertSize(sideDishChoice);
 
-        System.out.println("Enter one side dish (Calzone, Garlic bread, None):");
-        String sideDish = input.nextLine();
+        do{
+            System.out.println("Choose from one of the drinks below. We recommend Coca Cola:\r\n" + //
+                                " 1. Coca Cola\r\n" + //
+                                " 2. Cold coffee\r\n" + //
+                                " 3. Cocoa Drink\r\n" + //
+                                " 4. No drinks for me\r\n" + //
+                                " Enter your choice:");
+
+       
 
         System.out.println("Enter drinks(Cold Coffee, Cocoa drink, Coke, None):");
         String drinks = input.nextLine();
@@ -84,6 +147,54 @@ public class SliceoHeaven {
             input.close();
             
         }
+    
+    private static boolean isValidChoice(int choice) {
+        return choice >= 1 && choice <= 5;
+    }
+    private static boolean isValidChoice2(int choice) {
+        return choice >= 1 && choice <= 3;
+    }
+    private static String convertChoice(int choice) {
+        switch (choice) {
+        case 1:
+            return "Mushroom";
+        case 2:
+            return "Paprika";
+        case 3:
+            return "Sun - dried tomatoes";
+        case 4:
+            return "Chicken";
+        case 5:
+            return "Pineapple";
+        default:
+            return"";
+        }   
+       }
+       private static String convertSize(int choice) {
+        switch (choice) {
+        case 1:
+            return "Large";
+        case 2:
+            return "Medium";
+        case 3:
+            return "Small";
+     
+        }   
+    }
+    private static String convertSideChoice(int choice) {
+        switch (choice) {
+        case 1:
+            return "Calzone";
+        case 2:
+            return " Garlic bread";
+        case 3:
+            return "Chicken puff";
+        case 4:
+            return "Muffin";
+        case 5:
+            return "Nothing for me";
+        }   
+    }
         public void isItYourBirthday(){
             Scanner yourbirthday = new Scanner(System.in);
             
